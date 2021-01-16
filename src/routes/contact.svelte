873e94c509a21cf77c4aed977fe7cf8229358e1d
Phillip Malboeuf <phil@phils.computer>
2021-01-16 13:53:30 -0500

    contact page

---
+++ src/routes/contact.svelte
@@ -0,0 +1,80 @@
+<script context="module">
+
+	import { entry } from '$clients/contentful.svelte'
+
+	export async function load({ fetch }) {
+		const contact = await entry(fetch, 'contact', '6xck0ECuEVYO6N1UAGCp5D')
+		return { props: { contact } }
+	}
+</script>
+
+<script>
+  export let contact
+  const fields = Object.entries(contact.item.fields)
+</script>
+
+<section>
+  <h1>Contact</h1>
+
+  <ul>
+    {#each fields as [field, value]}
+    <li>
+      {#if field === 'adresse'}
+      <a href="https://www.google.com/maps/place/{value}" target="_blank">
+        {field}<br />
+        <span>{value}</span>
+      </a>
+      {:else if field === 'phone'}
+      <a href="tel:{value}" target="_blank">
+        Téléphone<br />
+        <span>{value}</span>
+      </a>
+      {:else if field === 'courriel' || field === 'emploi'}
+      <a href="mailto:{value}" target="_blank">
+        {field}<br />
+        <span>{value}</span>
+      </a>
+      {:else if field === 'suivre'}
+      <a href="https://instagram.com/{value}" target="_blank">
+        Nous suivre<br />
+        <span>@{value}</span>
+      </a>
+      {:else}
+      {field}<br />
+      <span>{value}</span>
+      {/if}
+    </li>
+    {/each}
+  </ul>
+</section>
+
+
+<style lang="scss">
+	section {
+		margin: 6rem 0;
+		display: grid;
+    grid-template-columns: 1fr 5fr;
+    column-gap: 2vw;
+	}
+
+  h1, ul {
+    font-size: 2rem;
+    line-height: 1.222;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+
+    li {
+      margin-bottom: 2vw;
+      text-transform: capitalize;
+    }
+
+    span {
+      opacity: 0.3;
+      text-transform: none;
+    }
+  }
+</style>
