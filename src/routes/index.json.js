c7cb275b5ae49ec9ab6e29806bdecc78a33e50a0
Phillip Malboeuf <phil@phils.computer>
2021-02-20 11:19:59 -0500

    cookie visits sequence

--- src/routes/index.json.js
+++ src/routes/index.json.js
@@ -1,3 +1,4 @@
+import cookie from 'cookie'
 import { entry, asset } from '../clients/contentful.js'
 
 const heroes = [
@@ -10,8 +11,10 @@ const heroes = [
 ]
 
 export async function get(req, res) {
+	const { visits } = cookie.parse(req.headers.cookie || '')
+
   const page = await entry('page', '2ZqXhnyr1p8XcClBDO3Lss', req.locale)
-	const hero = await asset(heroes[parseInt(Math.floor(Math.random() * heroes.length))])
+	const hero = await asset(heroes[visits ? parseInt(visits) % heroes.length : 0])
 
 	res.writeHead(200, {
 		'Content-Type': 'application/json'
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
+import cookie from 'cookie'
 import * as sapper from '@sapper/server';
 
 const { PORT, NODE_ENV } = process.env;
@@ -11,26 +12,26 @@ const locale = l => async (req, res, next) => {
 	next()
 }
 
+function session(req, res) {
+	const { visits } = cookie.parse(req.headers.cookie || '')
+	res.setHeader('Set-Cookie', cookie.serialize('visits', String(visits ? parseInt(visits) + 1 : 0), { httpOnly: true }))
+	return {
+		locale: req.locale
+	}
+}
+
 polka()
 	.use(
 		'/en',
 		locale('en'),
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
-		sapper.middleware({
-			session: (req, res) => ({
-				locale: req.locale
-			})
-		})
+		sapper.middleware({ session })
 	)
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
-		sapper.middleware({
-			session: (req, res) => ({
-				locale: req.locale
-			})
-		})
+		sapper.middleware({ session })
 	)
 	.listen(PORT, err => {
 		if (err) console.log('error', err);
